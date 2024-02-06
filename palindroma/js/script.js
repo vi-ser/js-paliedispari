1fcfb2129ad3d535b10bce8f61c95ad511212236
vi_ser <vinceulisse@gmail.com>
2024-02-06 15:58:46 +0100

    Esercizi conclusi

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -6,34 +6,46 @@ ATTENZIONE
 No .reverse, .split e .join.
 */
 
+// salvo gli elementi del DOM
+const resultEl = document.getElementById("result");
+
+// chiedo all'utente di scrivere una parola
 const userWord = prompt("Inserisci una parola");
 
-
-
-
-
+isPalindrome(userWord);
 
 // funzione per capire se una parola è palindroma
-// function isPalindrome(word) {
-let isPalindrome = true;
+function isPalindrome(word) {
 
-for (let i = 0; i < userWord.length; i++) {
-    if (userWord[i] != userWord[userWord.length - i - 1]) {
-        isPalindrome = false;
+    // inizializzo il flag su true
+    let isWordPalindrome = true;
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] != word[word.length - i - 1]) {
+
+            // se non è palindroma modifico il flag su false
+            isWordPalindrome = false;
+        }
     }
+
+    // stampo la parola al contrario
+    /*
+     for (let i = word.length - 1; i > -1; i--) {
+         console.log(word[i]);
+     }
+     */
+
+    console.log(word);
+
+    // se la parola è palindroma
+    if (isWordPalindrome) {
+        console.log("La parola è palindroma");
+        resultEl.innerHTML = `Hai scritto <strong>"${word}"</strong>.<br>La parola è palindroma!`;
+
+    } else {
+        // se non lo è
+        console.log("La parola non è palindroma");
+        resultEl.innerHTML = `Hai scritto <strong>"${word}"</strong>.<br>La parola non è palindroma.`;
+    }
+
 }
-
-for (let i = userWord.length - 1; i > -1; i--) {
-    console.log(userWord[i]);
-}
-
-if (isPalindrome) {
-    console.log(userWord);
-
-
-    console.log("La parola è palindroma")
-} else {
-    console.log("La parola non è palindroma");
-}
-
-// }
