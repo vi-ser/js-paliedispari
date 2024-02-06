fafe2eda1bdc1dc95ad480baba30ede0aefcdb51
vi_ser <vinceulisse@gmail.com>
2024-02-06 14:51:47 +0100

    esercizio pari o dispari concluso

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
 */
 
+// salvo gli elementi del DOM
+const yourNumberEl = document.getElementById("your-number");
+const pcNumberEl = document.getElementById("pc-number");
+
+const scoreEl = document.getElementById("score");
+const resultEl = document.getElementById("result");
+
+
+
+
 // salvo gli elementi bottoni
 const evenEl = document.getElementById("even");
 const oddEl = document.getElementById("odd");
@@ -21,15 +31,20 @@ evenEl.addEventListener("click",
 
         // numero scelto
         console.log(userNumber);
+        yourNumberEl.innerHTML = `Hai tirato ${userNumber}.`
+
 
         // numero scelto dal computer
         cpuNumber = randomNumber();
         console.log(cpuNumber);
+        pcNumberEl.innerHTML = `Il computer ha tirato ${cpuNumber}.`
 
 
         // sommo i due numeri e li memorizzo in una variabile
         let score = sum(userNumber, cpuNumber);
         console.log(score);
+        scoreEl.innerHTML = `La somma è ${score}.`
+
 
         // stabilisco se la somma è pari o dispari e la memorizzo in una variabile
         let result = isEvenOrOdd(score);
@@ -39,9 +54,12 @@ evenEl.addEventListener("click",
         // decreto vincita in caso di numero pari
         if (result == "even") {
             console.log("Pari! Hai vinto!");
+            resultEl.innerHTML = `Pari! Hai vinto!`
+
         }
         else {
             console.log("Hai perso!");
+            resultEl.innerHTML = `Dispari! Hai perso :c`
         }
 
     }
@@ -56,15 +74,18 @@ oddEl.addEventListener("click",
 
         // numero scelto
         console.log(userNumber);
+        yourNumberEl.innerHTML = `Hai tirato ${userNumber}.`
 
         // numero scelto dal computer
         cpuNumber = randomNumber();
         console.log(cpuNumber);
+        pcNumberEl.innerHTML = `Il computer ha tirato ${cpuNumber}.`
 
 
         // sommo i due numeri e li memorizzo in una variabile
         let score = sum(userNumber, cpuNumber);
         console.log(score);
+        scoreEl.innerHTML = `La somma è ${score}.`
 
         // stabilisco se la somma è pari o dispari e la memorizzo in una variabile
         let result = isEvenOrOdd(score);
@@ -74,8 +95,12 @@ oddEl.addEventListener("click",
         // decreto vincita in caso di numero dispari
         if (result == "odd") {
             console.log("Dispari! Hai vinto!");
+            resultEl.innerHTML = `Dispari! Hai vinto!`
+
         } else {
             console.log("Hai perso!");
+            resultEl.innerHTML = `Pari! Hai perso :c`
+
         }
 
     }
